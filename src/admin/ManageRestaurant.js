a49e5da773f8d38faea1365072d1777b06158502
MayurJ1609 <mayur.jaganmohan@gmail.com>
2020-06-16 22:44:20 +0530

    Added ManageRestaurant component

---
+++ src/admin/ManageRestaurant.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Base from "./../core/Base";
+
+const ManageRestaurant = () => {
+  return (
+    <Base title="Manage Restaurant" description="Mange all restaurants here">
+      <div></div>
+    </Base>
+  );
+};
+
+export default ManageRestaurant;
