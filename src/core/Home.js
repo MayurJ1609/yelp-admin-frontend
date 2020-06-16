2b532cd8c554cf76c95fcb1074e99795f5362023
MayurJ1609 <mayur.jaganmohan@gmail.com>
2020-06-16 22:43:43 +0530

    Added Home page component

---
+++ src/core/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Base from "./Base";
+
+const Home = () => {
+  return (
+    <Base title="Login or Sign Up" description="">
+      <div>Hi</div>
+    </Base>
+  );
+};
+
+export default Home;
